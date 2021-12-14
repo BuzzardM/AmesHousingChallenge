c39284dead7e2284e243893e42c528711d28f9c4
mikavaessen <85626985+mikavaessen@users.noreply.github.com>
2021-12-14 16:01:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+#%%
 import pandas as pd
 import seaborn as sns
 
 #%% Exercise 1
+df = pd.read_csv('res\AmesHousingData.csv', sep=';')
+dfCatigorical = pd.DataFrame()
+dfNumeric = pd.DataFrame()
+# %% 
+for col in df.columns:
+    if df[col].dtype == object:
+        dfCatigorical[col] = df[col]
+    else:
+        dfNumeric[col] = df[col]
+
+# %%
+dfNumeric.describe()
+# %%
+dfCatigorical.describe()
